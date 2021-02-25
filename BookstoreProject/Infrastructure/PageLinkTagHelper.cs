518c6b39e880f6b488004eb93f0dd8b453451dad
Emily Sandberg <emilywixom@gmail.com>
2021-02-24 21:02:01 -0700

    I added pagination in this version for assignment6

---
+++ BookstoreProject/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookstoreProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookstoreProject.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper //PageLinkTagHelper could have been named anything. Inherits from TagHelper class
+    {
+        private IUrlHelperFactory urlHelperFactory; //variable of the IUrlHelperFactory type
+
+        public PageLinkTagHelper (IUrlHelperFactory hp) //automatically called when object is created
+        {
+            urlHelperFactory = hp;
+        }
+
+        [ViewContext] //attributes
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; } //property
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        //Override process method
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
+                tag.InnerHtml.Append(i.ToString());
+
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
---
+++ BookstoreProject/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//creates class that allows us to do pagination and determine how many total pages we'll have
+namespace BookstoreProject.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalNumItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int)(Math.Ceiling((decimal)TotalNumItems / ItemsPerPage));
+    }
+}
